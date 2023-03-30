f3bd1c8a3a44e6d4ddc4d2825b6f356313ac4ca5
Vasily Maximov <softservice-31@mail.ru>
2023-03-30 07:15:09 +0300

    corrected java-filmorate

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -18,7 +18,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("/films")
-public class FilmController extends Controllers<Film>{
+public class FilmController extends Controllers<Film> {
 
     @PostMapping
     public Film createFilm(@Validated(CreateGroup.class) @RequestBody Film film) {
